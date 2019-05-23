7a771a94019bde0b8008d9c1da5f0643dc1a1602
informatica604 <48748469+informatica604@users.noreply.github.com>
2019-05-22 23:11:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+var imagen = new Array(24);
+
+function tipos(){
+    for(var k=0;k<25;k++){
+        imagen[k]=document.getElementsByTagName("img")[k];
+    }
+    imagen[0].addEventListener("mouseover",function(){imagen[0].width=250;imagen[0].height=250;},false);
+    imagen[0].addEventListener("mouseout",function(){imagen[0].width=200;imagen[0].height=200;},false);
+
+    imagen[1].addEventListener("mouseover",function(){imagen[1].width=250;imagen[1].height=250;},false);
+    imagen[1].addEventListener("mouseout",function(){imagen[1].width=200;imagen[1].height=200;},false);
+
+    imagen[2].addEventListener("mouseover",function(){imagen[2].width=250;imagen[2].height=250;},false);
+    imagen[2].addEventListener("mouseout",function(){imagen[2].width=200;imagen[2].height=200;},false);
+
+    imagen[3].addEventListener("mouseover",function(){imagen[3].width=250;imagen[3].height=250;},false);
+    imagen[3].addEventListener("mouseout",function(){imagen[3].width=200;imagen[3].height=200;},false);
+
+    imagen[4].addEventListener("mouseover",function(){imagen[4].width=250;imagen[4].height=250;},false);
+    imagen[4].addEventListener("mouseout",function(){imagen[4].width=200;imagen[4].height=200;},false);
+
+    imagen[5].addEventListener("mouseover",function(){imagen[5].width=250;imagen[5].height=250;},false);
+    imagen[5].addEventListener("mouseout",function(){imagen[5].width=200;imagen[5].height=200;},false);
+
+    imagen[6].addEventListener("mouseover",function(){imagen[6].width=250;imagen[6].height=250;},false);
+    imagen[6].addEventListener("mouseout",function(){imagen[6].width=200;imagen[6].height=200;},false);
+
+    imagen[7].addEventListener("mouseover",function(){imagen[7].width=250;imagen[7].height=250;},false);
+    imagen[7].addEventListener("mouseout",function(){imagen[7].width=200;imagen[7].height=200;},false);
+
+    imagen[8].addEventListener("mouseover",function(){imagen[8].width=250;imagen[8].height=250;},false);
+    imagen[8].addEventListener("mouseout",function(){imagen[8].width=200;imagen[8].height=200;},false);
+
+    imagen[9].addEventListener("mouseover",function(){imagen[9].width=250;imagen[9].height=250;},false);
+    imagen[9].addEventListener("mouseout",function(){imagen[9].width=200;imagen[9].height=200;},false);
+
+    imagen[10].addEventListener("mouseover",function(){imagen[10].width=250;imagen[10].height=250;},false);
+    imagen[10].addEventListener("mouseout",function(){imagen[10].width=200;imagen[10].height=200;},false);
+
+    imagen[11].addEventListener("mouseover",function(){imagen[11].width=250;imagen[11].height=250;},false);
+    imagen[11].addEventListener("mouseout",function(){imagen[11].width=200;imagen[11].height=200;},false);
+
+    imagen[12].addEventListener("mouseover",function(){imagen[12].width=250;imagen[12].height=250;},false);
+    imagen[12].addEventListener("mouseout",function(){imagen[12].width=200;imagen[12].height=200;},false);
+
+    imagen[13].addEventListener("mouseover",function(){imagen[13].width=250;imagen[13].height=250;},false);
+    imagen[13].addEventListener("mouseout",function(){imagen[13].width=200;imagen[13].height=200;},false);
+
+    imagen[14].addEventListener("mouseover",function(){imagen[14].width=250;imagen[14].height=250;},false);
+    imagen[14].addEventListener("mouseout",function(){imagen[14].width=200;imagen[14].height=200;},false);
+
+    imagen[15].addEventListener("mouseover",function(){imagen[15].width=250;imagen[15].height=250;},false);
+    imagen[15].addEventListener("mouseout",function(){imagen[15].width=200;imagen[15].height=200;},false);
+
+    imagen[16].addEventListener("mouseover",function(){imagen[16].width=250;imagen[16].height=250;},false);
+    imagen[16].addEventListener("mouseout",function(){imagen[16].width=200;imagen[16].height=200;},false);
+
+    imagen[17].addEventListener("mouseover",function(){imagen[17].width=250;imagen[17].height=250;},false);
+    imagen[17].addEventListener("mouseout",function(){imagen[17].width=200;imagen[17].height=200;},false);
+
+    imagen[18].addEventListener("mouseover",function(){imagen[18].width=250;imagen[18].height=250;},false);
+    imagen[18].addEventListener("mouseout",function(){imagen[18].width=200;imagen[18].height=200;},false);
+
+    imagen[19].addEventListener("mouseover",function(){imagen[19].width=250;imagen[19].height=250;},false);
+    imagen[19].addEventListener("mouseout",function(){imagen[19].width=200;imagen[19].height=200;},false);
+
+    imagen[20].addEventListener("mouseover",function(){imagen[20].width=250;imagen[20].height=250;},false);
+    imagen[20].addEventListener("mouseout",function(){imagen[20].width=200;imagen[20].height=200;},false);
+
+    imagen[21].addEventListener("mouseover",function(){imagen[21].width=250;imagen[21].height=250;},false);
+    imagen[21].addEventListener("mouseout",function(){imagen[21].width=200;imagen[21].height=200;},false);
+
+    imagen[22].addEventListener("mouseover",function(){imagen[22].width=250;imagen[22].height=250;},false);
+    imagen[22].addEventListener("mouseout",function(){imagen[22].width=200;imagen[22].height=200;},false);
+
+    imagen[23].addEventListener("mouseover",function(){imagen[23].width=250;imagen[23].height=250;},false);
+    imagen[23].addEventListener("mouseout",function(){imagen[23].width=200;imagen[23].height=200;},false);
+
+    imagen[24].addEventListener("mouseover",function(){imagen[24].width=250;imagen[24].height=250;},false);
+    imagen[24].addEventListener("mouseout",function(){imagen[24].width=200;imagen[24].height=200;},false);
+}
+
+window.addEventListener("load",tipos,false);
+
